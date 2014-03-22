d9e6dda5b409f93e4d8e148da3afb009fbfbf99b
Mathias Gawlista <gawlista.mathias@vimn.com>
2014-03-22 11:21:49 +0100

    Add product to workflow user navigation.

--- lib/voluntary_text_creation/engine.rb
+++ lib/voluntary_text_creation/engine.rb
@@ -1,4 +1,9 @@
 module VoluntaryTextCreation
   class Engine < ::Rails::Engine
+    config.i18n.load_path += Dir[File.expand_path("../../../config/locales/**/*.{rb,yml}", __FILE__)]
+    
+    config.to_prepare do
+      Voluntary::Navigation::Base.add_product('text-creation', 'workflow.user.products.text_creation.title')
+    end
   end
 end
