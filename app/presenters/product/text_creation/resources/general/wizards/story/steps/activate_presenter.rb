a32a94b137120e623701712d985c97e3b7fbc54b
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-03-31 18:59:59 +0200

    Handle warning 'toplevel constant Resources referenced by Product::TextCreation::Resources'

--- app/presenters/product/text_creation/resources/general/wizards/story/steps/activate_presenter.rb
+++ app/presenters/product/text_creation/resources/general/wizards/story/steps/activate_presenter.rb
@@ -1,3 +1,17 @@
-class Product::TextCreation::Resources::General::Wizards::Story::Steps::ActivatePresenter < Presenter
-  def task_columns; ['name', 'keywords']; end
+class Product
+  class TextCreation
+    module Resources
+      module General
+        module Wizards
+          module Story
+            module Steps
+              class ActivatePresenter < Presenter
+                def task_columns; ['name', 'keywords']; end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 end
--- app/presenters/product/text_creation/resources/general/wizards/story/steps/setup_tasks_presenter.rb
+++ app/presenters/product/text_creation/resources/general/wizards/story/steps/setup_tasks_presenter.rb
@@ -1,5 +1,19 @@
-class Product::TextCreation::Resources::General::Wizards::Story::Steps::SetupTasksPresenter < Presenter
-  def form_options
-    { html: {class: 'floating_form form-vertical'}, url: story_path(resource), as: :story }
+class Product
+  class TextCreation
+    module Resources
+      module General
+        module Wizards
+          module Story
+            module Steps
+              class SetupTasksPresenter < Presenter
+                def form_options
+                  { html: {class: 'floating_form form-vertical'}, url: story_path(resource), as: :story }
+                end
+              end
+            end
+          end
+        end
+      end
+    end
   end
 end
