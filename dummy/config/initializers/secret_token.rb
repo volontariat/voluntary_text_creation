6fb4ebe0456bb26eb7a00fb9cefca64908dd5205
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-04-09 18:49:47 +0200

    Renames config secret token to secret key base.

--- dummy/config/initializers/secret_token.rb
+++ dummy/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'd6761116ac8b3dc6385cb9a004963f544f7b968d771f717e16ec55f2f7602dda3635f93bd7e0b7f8f2a36cc8ea3508afbecec3e731525e07999df2a19691073f'
+Dummy::Application.config.secret_key_base = 'd6761116ac8b3dc6385cb9a004963f544f7b968d771f717e16ec55f2f7602dda3635f93bd7e0b7f8f2a36cc8ea3508afbecec3e731525e07999df2a19691073f'
